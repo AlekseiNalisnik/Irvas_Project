ac9825a8a40622bfe81541a93abef81b58b2205f
Alex <mkumum12@mail.ru>
2020-03-29 21:56:25 +0300

    Added forms(valudation + server)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
+// const dist = 'C:/MAMP/htdocs/JS_first_project';
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
